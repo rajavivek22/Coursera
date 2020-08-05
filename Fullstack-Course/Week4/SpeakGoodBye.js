6ad0c992521bb56c6d8aae99015a09ba71b3f287
Raja Vivek S <62027216+rajavivek22@users.noreply.github.com>
2020-08-05 20:43:16 +0530

    Add files via upload

---
+++ Fullstack-Course/Week4/SpeakGoodBye.js
@@ -0,0 +1,13 @@
+(function (window) {    
+  var byeSpeaker={};
+  byeSpeaker.speakWord = "Good Bye";
+
+  byeSpeaker.speak = function(name) {
+  console.log(byeSpeaker.speakWord + " " + name);
+}
+
+window.byeSpeaker=byeSpeaker;
+
+})(window);
+
+
